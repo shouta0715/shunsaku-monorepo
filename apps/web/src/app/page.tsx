10b5a15e416ed51ee3e0063817c1b2677a25d717
shouta0715 <s_kurahashi@yumemi.co.jp>
2025-08-03 17:17:06 +0900

    feat: add tailwind animate

--- apps/web/src/app/page.tsx
+++ apps/web/src/app/page.tsx
@@ -1,15 +1,3 @@
-import { Heading } from "@package/ui";
-
 export default function Home() {
-  return (
-    <div className="grid min-h-screen place-items-center p-8">
-      <main className="text-center">
-        <h1 className="mb-8 text-4xl font-bold">Welcome to Next.js</h1>
-        <p className="text-lg text-gray-600">
-          Start building your amazing application.
-        </p>
-        <Heading level={1}>Hello</Heading>
-      </main>
-    </div>
-  );
+  return <div>Hello</div>;
 }
