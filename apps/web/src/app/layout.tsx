bdf9c9f2dcd76a6b9f646e8abb0051cc6774bb59
shouta0715 <s_kurahashi@yumemi.co.jp>
2025-08-04 19:44:33 +0900

    fix: SEO

--- apps/web/src/app/layout.tsx
+++ apps/web/src/app/layout.tsx
@@ -20,9 +20,8 @@ const geistMono = Geist_Mono({
 });
 
 export const metadata: Metadata = {
-  title: "離職リスク予測ダッシュボード",
-  description:
-    "日々の声から、未来の離職をゼロへ。社員の満足度を可視化し、離職リスクを早期発見するHRダッシュボード",
+  title: "やめどき予報",
+  description: "日々の声から、未来の離職をゼロへ。社員の満足度を可視化し、離職リスクを早期発見するHRダッシュボード",
 };
 
 type RootLayoutProps = {
